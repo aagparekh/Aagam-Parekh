4110ce98d9c637da41c9e07377113154f8232ec1
Aagam Parekh <aagparekh0430@gmail.com>
2025-07-16 22:37:36 +0530

    recent changes

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <div className='d-flex align-items-center justify-content-center' style={{ height: '600px',backgroundImage: `url(${tunnel})`, backgroundRepeat: 'no-repeat, repeat', backgroundSize: 'cover',backgroundPosition: 'center' }}>
             <div style={{ height: '250px', width: '250px', color: 'black'}}>
               <h1 style={{letterSpacing: '3.5px'}} className='text-white fw-bolder'> I'M<br/>AAGAM<br/>PAREKH.</h1>
-              <p className='text-white fw-semibold'>Student & Aspiring </p>
+              <p className='text-white fw-semibold'> </p>
               <div data-toggle="tooltip" data-placement="bottom" title="Aagam Parekh"><a href="https://bit.ly/3bPb4yD"><i class="fa-brands fa-linkedin fa-3x"></i></a>
               <a href="https://bit.ly/3AFymBc"><i class="fa-brands fa-hackerrank fa-3x text-success mx-4"></i></a>
               <a href="https://github.com/aagparekh"><i class="fa-brands fa-github fa-3x"></i></a>
